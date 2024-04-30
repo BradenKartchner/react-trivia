68ef5f8199ce2bc32d4ecea520729820fb583f74
Braden Kartchner <62575814+BradenKartchner@users.noreply.github.com>
2024-04-30 16:50:40 -0600

    basic card much more customizable in bootstrap than reactstrap

---
+++ vite-project/src/components/BootCard.tsx
@@ -0,0 +1,37 @@
+// use bootstrap or reactstrap? maybe just try bootstrap first
+// see this link: https://www.tutorialrepublic.com/twitter-bootstrap-tutorial/bootstrap-grid-system.php
+// and scroll down a bit to see how to structure a website to work on multiple different size screens
+
+function BootCard() {
+    return (
+        <>
+            <div className="card" style={{ width: "22rem" }}>
+                <div className="card-body">
+                    <h5 className="card-title">Card title here</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                        card subtitle here
+                    </h6>
+                    <p className="card-text">
+                        The main text of the card can go here... Does the text
+                        begin to wrap as we get closer to 22rem of width? Yes it
+                        does!!
+                    </p>
+                    <button type="button" className="btn btn-primary">
+                        Primary
+                    </button>
+                    <button type="button" className="btn btn-secondary">
+                        Secondary
+                    </button>
+                    <button type="button" className="btn btn-success">
+                        Success
+                    </button>
+                    <button type="button" className="btn btn-outline-danger">
+                        Danger
+                    </button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default BootCard;
