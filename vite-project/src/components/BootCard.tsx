9b0ef9d1d81cded2927e5f1e49d668dcae7998f2
Braden Kartchner <62575814+BradenKartchner@users.noreply.github.com>
2024-05-06 16:23:43 -0600

    more css shenanigans

--- vite-project/src/components/BootCard.tsx
+++ vite-project/src/components/BootCard.tsx
@@ -7,11 +7,7 @@ import "./BootCard.css";
 function BootCard() {
     return (
         <>
-            <div
-                className="card"
-                style={{ width: "22rem" }}
-                data-bs-theme="dark"
-            >
+            <div className="card" data-bs-theme="dark">
                 <div className="card-body">
                     <h5 className="card-title">Card title here</h5>
                     <h6 className="card-subtitle mb-2 text-muted">
@@ -22,18 +18,23 @@ function BootCard() {
                         begin to wrap as we get closer to 22rem of width? Yes it
                         does!!
                     </p>
-                    <button type="button" className="btn btn-primary">
-                        Primary
-                    </button>
-                    <button type="button" className="btn btn-secondary">
-                        Secondary
-                    </button>
-                    <button type="button" className="btn btn-success">
-                        Success
-                    </button>
-                    <button type="button" className="btn btn-outline-danger">
-                        Danger
-                    </button>
+                    <p className="card-text">
+                        <button type="button" className="btn btn-primary">
+                            Primary
+                        </button>
+                        <button type="button" className="btn btn-secondary">
+                            Secondary
+                        </button>
+                        <button type="button" className="btn btn-success">
+                            Success
+                        </button>
+                        <button
+                            type="button"
+                            className="btn btn-outline-danger"
+                        >
+                            Danger
+                        </button>
+                    </p>
                 </div>
             </div>
         </>
