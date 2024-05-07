f4cab7eb7c8c0a724dfbb87c9f67f29c0209aced
Braden Kartchner <62575814+BradenKartchner@users.noreply.github.com>
2024-05-07 17:07:48 -0600

    quiz card progress bar added

--- vite-project/src/components/QuizCard.tsx
+++ vite-project/src/components/QuizCard.tsx
@@ -1,7 +1,6 @@
 /**
- * TODO: add buttons to scroll back and forth between questions
- * TODO: add section to bottom of card with score and progress bar
  * TODO: add button to the top left(?) to return to quiz menu
+ * TODO: map over eventual array of questions answered to return progress bar segments
  */
 
 import "./QuizCard.css";
@@ -9,6 +8,7 @@ import "./QuizCard.css";
 function QuizCard() {
     const leftArrow = String.fromCodePoint(0x21e6);
     const rightArrow = String.fromCodePoint(0x21e8);
+    const sampleWidth = "15%";
     return (
         <>
             <div className="card" data-bs-theme="dark">
@@ -45,6 +45,37 @@ function QuizCard() {
                             Answer 4
                         </button>
                     </p>
+                    <p className="card-text score-container">Score: 0/20</p>
+                    <div className="progress-stacked my-progress">
+                        <div
+                            className="progress"
+                            role="progressbar"
+                            aria-label="Segment one"
+                            aria-valuenow={15}
+                            aria-valuemin={0}
+                            aria-valuemax={100}
+                            style={{ width: sampleWidth }}
+                        >
+                            <div
+                                className="progress-bar bg-success"
+                                style={{ height: "1rem" }}
+                            ></div>
+                        </div>
+                        <div
+                            className="progress"
+                            role="progressbar"
+                            aria-label="Segment two"
+                            aria-valuenow={15}
+                            aria-valuemin={0}
+                            aria-valuemax={100}
+                            style={{ width: sampleWidth }}
+                        >
+                            <div
+                                className="progress-bar"
+                                style={{ height: "1rem" }}
+                            ></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
